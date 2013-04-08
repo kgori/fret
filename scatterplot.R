d0646f651969dd8f0fd15210ee489c9badef921e
Kevin Gori <kgori@ebi.ac.uk>
2013-04-08 10:49:56 +0100

    scatterplot.R -- code for generating human-ferret vs human-mouse genetic distance scatterplots

---
+++ scatterplot.R
@@ -0,0 +1,29 @@
+mypl <- function(d, xlab="", ylab="", title="") {
+  require(ggplot2)
+  colours <- c(
+  	"#1D2690", "#3040F0", "#838CF6", #blues
+  	"#137b6a", "#20CDB0", "#79e1d0", 
+  	"#6a9013", "#B0F020", "#d0f679", 
+  	"#906a13", "#F0B020", "#f6d079")
+  labels <- c(
+  	"a", "b", "c",
+  	"d", "e", "f",
+  	"g", "h", "i", 
+  	"j", "k", "l")
+  ferret_dist = d$HUMAN.FERRET.DIST
+  mouse_dist = d$HUMAN.MOUSE.DIST
+  
+  groups = factor(paste(d$QUANTS, d$MAGQ), labels=labels)
+  axlim = max(ferret_dist, mouse_dist)
+  
+  p <- ggplot(pam, aes(x=ferret_dist, y=mouse_dist, colour=groups), environment=environment()) + 
+    geom_point() + 
+    xlim(0,axlim) + 
+    ylim(0,axlim) +
+    coord_fixed() +
+    labs(title=title, x="vs Ferret  /PAM", y="vs Mouse  /PAM") +
+    geom_abline(colour='red', linetype='dashed', size=0.5) +
+    scale_colour_manual(values = colours) +
+    theme_bw()
+  return(p)
+}
