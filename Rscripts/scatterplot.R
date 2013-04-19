e1a40b7af782ef655b31f3c6b52f9a6dd2894e51
kgori <kgori@ebi.ac.uk>
2013-04-19 14:52:16 +0100

    Changes to scatterplot.R

--- Rscripts/scatterplot.R
+++ Rscripts/scatterplot.R
@@ -1,22 +1,24 @@
-mypl <- function(d, xlab="", ylab="", title="") {
+mypl <- function(d, datatype="dna", xlab="", ylab="", title="") {
   require(ggplot2)
   colours <- c(
   	"#1D2690", "#3040F0", "#838CF6", #blues
-  	"#137b6a", "#20CDB0", "#79e1d0", 
-  	"#6a9013", "#B0F020", "#d0f679", 
-  	"#906a13", "#F0B020", "#f6d079")
+  	"#137b6a", "#20CDB0", "#79e1d0", #cyans
+  	"#6a9013", "#B0F020", "#d0f679", #greens
+  	"#906a13", "#F0B020", "#f6d079") #oranges
   labels <- c( #placeholders
-  	"a", "b", "c",
-  	"d", "e", "f",
-  	"g", "h", "i", 
-  	"j", "k", "l")
-  ferret_dist = d$HUMAN.FERRET.DIST
-  mouse_dist = d$HUMAN.MOUSE.DIST
+  	"0-25%, slowest third", "0-25%, middle third", "0-25%, fastest third",
+  	"25-50%, slowest third", "25-50%, middle third", "25-50%, fastest third",
+  	"50-75%, slowest third", "50-75%, middle third", "50-75%, fastest third", 
+  	"75-100%, slowest third", "75-100%, middle third", "75-100%, fastest third")
   
-  groups = factor(paste(d$QUANTS, d$MAGQ), labels=labels)
-  axlim = max(ferret_dist, mouse_dist)
+  data <- subset(d, DATATYPE==datatype)
+  ferret_dist <- data$HUMAN.FERRET.DIST
+  mouse_dist <- data$HUMAN.MOUSE.DIST
   
-  p <- ggplot(pam, aes(x=ferret_dist, y=mouse_dist, colour=groups), environment=environment()) + 
+  groups <- factor(paste(data$QUANTS, data$MAGQ), labels=labels)
+  axlim <- max(ferret_dist, mouse_dist)
+  
+  p <- ggplot(data, aes(x=ferret_dist, y=mouse_dist, colour=groups), environment=environment()) + 
     geom_point() + 
     xlim(0,axlim) + 
     ylim(0,axlim) +
